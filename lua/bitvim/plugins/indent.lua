c5f72737d39e6c8786b98481d3df09c6446121a7
BitSwapped <228873900+BitSwapped@users.noreply.github.com>
2025-10-29 20:54:19 -0500

    feat: add indent-blankline mini-indentscope

---
+++ lua/bitvim/plugins/indent.lua
@@ -0,0 +1,27 @@
+return { 
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "VeryLazy",
+    main = "ibl",
+    opts = {
+      indent = {
+        char = "│",
+      },
+      scope = {
+        enabled = false,
+      },
+      exclude = {
+        filetypes = { "help", "alpha", "dashboard", "Trouble", "lazy", "neo-tree" },
+      },
+      whitespace = {
+        remove_blankline_trail = true,
+      },
+    }
+  },
+  { 
+    'nvim-mini/mini.indentscope',
+    events = "VeryLazy",
+    version = false,
+    opts = {}
+  }
+}
